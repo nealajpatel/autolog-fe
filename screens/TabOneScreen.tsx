9bd069732baac367730bd0c71653ad3ae6262656
Nikhil Shirsekar <nshirsekar72@Nikhils-MBP.attlocal.net>
2020-08-29 18:17:13 -0400

    Updated page to be garage and to collapsable cards, width of tabs is still hardcoded for the time being. working to get this to be dynamic to the device

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -2,14 +2,15 @@ import * as React from 'react';
 import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
+import GarageTabs from '../components/GarageTabs'
 import { Text, View } from '../components/Themed';
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+      <Text style={styles.title}>Garage</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <GarageTabs/>
     </View>
   );
 }
@@ -18,14 +19,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    // justifyContent: 'center',
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+    paddingTop: 15
   },
   separator: {
-    marginVertical: 30,
+    marginVertical: 15,
     height: 1,
     width: '80%',
   },
